aa54c64321583e5dc6ce310aea047c2a0f599f85
crizostomo <diogo_crizostomo@hotmail.com>
2024-03-11 21:23:57 -0300

    feat: [HTML] Knowledge Component

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IKnowledge } from '../../interface/iknowledge.interface';
 
 @Component({
   selector: 'app-knowledge',
@@ -9,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class KnowledgeComponent {
 
+  public arrayKnowledge = signal<IKnowledge[]>([
+    { src: 'assets/icons/knowledge/java.svg',
+      alt: 'java Knowledge'
+    },
+    { src: 'assets/icons/knowledge/spring.svg',
+      alt: 'spring Knowledge'
+    },
+    { src: 'assets/icons/knowledge/angular.svg',
+      alt: 'angular Knowledge'
+    },
+    { src: 'assets/icons/knowledge/html5.svg',
+      alt: 'html5 Knowledge'
+    },
+    { src: 'assets/icons/knowledge/css3.svg',
+      alt: 'css3 Knowledge'
+    }
+  ])
 }
