251162339bb1fa1dc124aaf77dc3c46fae6628ca
crizostomo <diogo_crizostomo@hotmail.com>
2024-03-13 13:46:43 -0300

    feat: [HTML] Projects Component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/iprojects.interface';
 
 @Component({
   selector: 'app-projects',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/github.png',
+      alt: "Github Projects",
+      title: "Github Projects",
+      width: '100px',
+      height: '100px',
+      description: '<p>Access my Github to check my projects</p>',
+      links: [
+        {
+          name: 'Get to know my Github',
+          href: 'https://github.com/crizostomo',
+        },
+      ],
+    },
+  ]);
 }
